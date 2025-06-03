d0973137c10f54b3751d95fab99bd0f05d9dc30d
popo <longjialing0305@gmail.com>
2025-06-03 20:15:07 +0800

    Update testing.java

--- src/main/java/com/mycompany/addnumbers/testing.java
+++ src/main/java/com/mycompany/addnumbers/testing.java
@@ -149,7 +149,7 @@ public class testing{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         
-        System.out.print("Your age: ");
+        System.out.print("Enter age: ");
         int ageOfEntrant=sc.nextInt();
         double entryFee;
         
